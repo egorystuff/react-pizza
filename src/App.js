6ba7fe6d3300bf0afdda9607961049783cb57610
Yahor Yankavets <mr.yankovec@gmail.com>
2023-07-20 19:45:47 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
@@ -11,7 +13,11 @@ function App() {
 			<Header />
 			<div className="content">
 				<div className="container">
-					<NotFound />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="cart" element={<Cart />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
 				</div>
 			</div>
 		</div>
